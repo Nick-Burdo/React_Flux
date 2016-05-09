129e76a31f4e4fb409507388700ea60cc6eb55f4
Nick Burdo <n.burdo@iterios.com>
2016-05-09 22:25:52 +0300

    add Bootstrap theme

--- src/locale/en.js
+++ src/locale/en.js
@@ -6,7 +6,7 @@ module.exports = {
             male: {
                 zero: '%(name)s do not added notifications to their note book.',
                 one: '%(name)s added one notification to their note book.',
-                other: '%(name)s added %(count1)s notifications to their note book.'
+                other: '%(name)s added %(count)s notifications to their note book.'
             },
             female: {
                 zero: '%(name)s do not added notifications to their note book.',
